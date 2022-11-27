6f18cb1d4a764062bb40ee4eabc2125a22a9d90b
mrSemyon <syoma4k@gmail.com>
2022-11-27 23:06:55 +0500

    Added project core

---
+++ app/init.php
@@ -0,0 +1,6 @@
+<?php
+session_start();
+
+require $_SERVER['DOCUMENT_ROOT'] . '/app/classes.php';
+require $_SERVER['DOCUMENT_ROOT'] . '/app/config.php';
+require $_SERVER['DOCUMENT_ROOT'] . '/app/functions.php';
