871f2a8fbfc45739c239de19e04a99227792b645
mrSemyon <syoma4k@gmail.com>
2022-11-27 23:10:51 +0500

    Added class for DB connection

---
+++ app/classes.php
@@ -0,0 +1,2 @@
+<?php
+require $_SERVER['DOCUMENT_ROOT'] . '/app/classes/Connection.php';
---
+++ app/classes/Connection.php
@@ -0,0 +1,14 @@
+<?php
+
+class Connection
+{
+    public static function make($config)
+    {
+        return new PDO(
+            "mysql:host={$config['host']};dbname={$config['name']};charset={$config['charset']}",
+            $config['user'],
+            $config['password'],
+            $config['opt']
+        );
+    }
+}
