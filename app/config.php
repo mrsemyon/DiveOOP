2029793282dcbe029d4ad3abfd0a7220cd53a2d5
mrSemyon <syoma4k@gmail.com>
2022-11-28 23:08:36 +0500

    Added configuration file

---
+++ app/config.php
@@ -0,0 +1,16 @@
+<?php
+
+$config = [
+    'mysql' => [
+        'host'      => '127.0.0.1',
+        'name'      => 'dive',
+        'user'      => 'root',
+        'password'  => 'root',
+        'charset'   => 'utf8',
+        'opt'       => [
+            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES   => false,
+        ],
+    ],
+];
