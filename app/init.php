d1b692d90d5e9a6df02c4ece78a32f7661466708
mrSemyon <syoma4k@gmail.com>
2022-11-28 23:13:19 +0500

    Added creating QueryBuilder instance

--- app/init.php
+++ app/init.php
@@ -1,6 +1,9 @@
 <?php
+
 session_start();
 
 require $_SERVER['DOCUMENT_ROOT'] . '/../app/classes.php';
 require $_SERVER['DOCUMENT_ROOT'] . '/../app/config.php';
 require $_SERVER['DOCUMENT_ROOT'] . '/../app/functions.php';
+
+$db = new QueryBuilder(Connection::make($config['mysql']));
