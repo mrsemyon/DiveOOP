3a87e9b7ec1120708bcccbf7d94c37247e477409
mrSemyon <syoma4k@gmail.com>
2022-11-28 23:51:22 +0500

    Added read() method to QueryBuilder class

--- app/classes/QueryBuilder.php
+++ app/classes/QueryBuilder.php
@@ -3,8 +3,27 @@
 class QueryBuilder
 {
     protected $pdo;
-    public function __construct($pdo)
+    public function __construct(PDO $pdo)
     {
         $this->pdo = $pdo;
+    }
+
+    public function read(string $table, array $where = NULL)
+    {
+        if ($where) {
+            $sql = "SELECT * FROM $table WHERE ";
+            foreach ($where as $key => $value) {
+                $sql .= $key . '=:' . $key . ' AND ';
+            }
+            $sql = rtrim($sql, ' AND ') . ';';
+            $statement = $this->pdo->prepare($sql);
+            $statement->execute($where);
+            return $statement->fetch();
+        } else {
+            $sql = "SELECT * FROM $table";
+            $statement = $this->pdo->prepare($sql);
+            $statement->execute();
+            return $statement->fetchAll();
+        }
     }
 }
