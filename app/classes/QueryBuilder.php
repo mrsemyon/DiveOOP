0f34e15a4252d2da341ac13dbb7652537e3c1632
mrSemyon <syoma4k@gmail.com>
2022-11-29 17:03:46 +0500

    Added create() method to QueryBuilder class

--- app/classes/QueryBuilder.php
+++ app/classes/QueryBuilder.php
@@ -3,6 +3,7 @@
 class QueryBuilder
 {
     protected $pdo;
+
     public function __construct(PDO $pdo)
     {
         $this->pdo = $pdo;
@@ -25,5 +26,15 @@ class QueryBuilder
             $statement->execute();
             return $statement->fetchAll();
         }
+    }
+
+    public function create(string $table, array $data): int
+    {
+        $keys = implode(', ', array_keys($data));
+        $values = ':' . implode(', :', array_keys($data));
+        $sql = "INSERT INTO $table ($keys) VALUES ($values)";
+        $statement = $this->pdo->prepare($sql);
+        $statement->execute($data);
+        return $this->pdo->lastInsertId();
     }
 }
