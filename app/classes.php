461201029818fbae0821342821a8b4d09cb13960
mrSemyon <syoma4k@gmail.com>
2022-11-30 23:51:24 +0500

    Added Token class

--- app/classes.php
+++ app/classes.php
@@ -4,3 +4,4 @@ require $_SERVER['DOCUMENT_ROOT'] . '/app/classes/Connection.php';
 require $_SERVER['DOCUMENT_ROOT'] . '/app/classes/QueryBuilder.php';
 require $_SERVER['DOCUMENT_ROOT'] . '/app/classes/Redirect.php';
 require $_SERVER['DOCUMENT_ROOT'] . '/app/classes/Session.php';
+require $_SERVER['DOCUMENT_ROOT'] . '/app/classes/Token.php';
