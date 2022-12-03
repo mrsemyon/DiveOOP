056ccab63901df0e5fa3731ac0a64f72fcaf1495
mrSemyon <syoma4k@gmail.com>
2022-12-04 01:11:42 +0500

    Revert "Introduced the singleton pattern in the QueryBuilder class"
    This reverts commit 3ff4fd601fef0f8141bc761089c2d5684aad9c47.

--- app/classes/QueryBuilder.php
+++ app/classes/QueryBuilder.php
@@ -2,51 +2,39 @@
 
 class QueryBuilder
 {
-    private static $instance = null;
-    private static $pdo;
+    protected $pdo;
 
-    public function __construct()
+    public function __construct($pdo)
     {
-        self::$pdo = Connection::getInstance()->pdo();
+        $this->pdo = $pdo;
     }
 
-    public static function getInstance()
+    public function read(string $table, array $where = NULL)
     {
-        if (!isset(self::$instance)) {
-
-            self::$instance = new self;
-        }
-        return self::$instance;
-    }
-
-    public static function read(string $table, array $where = NULL)
-    {
-        self::getInstance();
         if ($where) {
             $sql = "SELECT * FROM $table WHERE ";
             foreach ($where as $key => $value) {
                 $sql .= $key . '=:' . $key . ' AND ';
             }
             $sql = rtrim($sql, ' AND ') . ';';
-            $statement = self::$pdo->prepare($sql);
+            $statement = $this->pdo->prepare($sql);
             $statement->execute($where);
             return $statement->fetch();
         } else {
             $sql = "SELECT * FROM $table";
-            $statement = self::$pdo->prepare($sql);
+            $statement = $this->pdo->prepare($sql);
             $statement->execute();
             return $statement->fetchAll();
         }
     }
 
-    public static function create(string $table, array $data): int
+    public function create(string $table, array $data): int
     {
-        self::getInstance();
         $keys = implode(', ', array_keys($data));
         $values = ':' . implode(', :', array_keys($data));
         $sql = "INSERT INTO $table ($keys) VALUES ($values)";
-        $statement = self::$pdo->prepare($sql);
+        $statement = $this->pdo->prepare($sql);
         $statement->execute($data);
-        return self::$pdo->lastInsertId();
+        return $this->pdo->lastInsertId();
     }
 }
--- app/init.php
+++ app/init.php
@@ -5,3 +5,5 @@ session_start();
 require $_SERVER['DOCUMENT_ROOT'] . '/app/classes.php';
 require $_SERVER['DOCUMENT_ROOT'] . '/app/config.php';
 require $_SERVER['DOCUMENT_ROOT'] . '/app/functions.php';
+
+$db = new QueryBuilder(Connection::getInstance()->pdo());
