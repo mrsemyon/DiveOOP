3ff4fd601fef0f8141bc761089c2d5684aad9c47
mrSemyon <syoma4k@gmail.com>
2022-12-03 21:54:41 +0500

    Introduced the singleton pattern in the QueryBuilder class

--- app/init.php
+++ app/init.php
@@ -5,5 +5,3 @@ session_start();
 require $_SERVER['DOCUMENT_ROOT'] . '/app/classes.php';
 require $_SERVER['DOCUMENT_ROOT'] . '/app/config.php';
 require $_SERVER['DOCUMENT_ROOT'] . '/app/functions.php';
-
-$db = new QueryBuilder(Connection::getInstance()->pdo());
