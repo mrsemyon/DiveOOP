8436a4427f6d0a8f4fb8208a6bce40ba0755bd9a
mrSemyon <syoma4k@gmail.com>
2022-12-06 18:28:48 +0500

    Added controller for create user

---
+++ controllers/create.php
@@ -0,0 +1,41 @@
+<?php
+require $_SERVER['DOCUMENT_ROOT'] . '/app/init.php';
+
+if (!empty(QueryBuilder::getInstance()->read('users', ['email' => $_POST['email']]))) {
+    Session::flash('danger', 'Этот эл. адрес уже занят другим пользователем.');
+    Redirect::to('/public/registration');
+    exit;
+}
+
+$id = QueryBuilder::getInstance()->create(
+    'users',
+    [
+        'email'     => $_POST['email'],
+        'password'  => password_hash($_POST['password'], PASSWORD_DEFAULT),
+        'role'      => 'user',
+    ]
+);
+
+$photo = (!empty($_FILES['photo']['name']))
+    ? prepareUserPhoto($_FILES['photo'])
+    : 'no_photo.jpg';
+
+QueryBuilder::getInstance()->update(
+    'users',
+    [
+        'name'      => $_POST['name'],
+        'position'  => $_POST['position'],
+        'phone'     => $_POST['phone'],
+        'address'   => $_POST['address'],
+        'status'    => $_POST['status'],
+        'photo'     => $photo,
+        'vk'        => $_POST['vk'],
+        'tg'        => $_POST['tg'],
+        'ig'        => $_POST['ig']
+    ],
+    ['id' => $id]
+);
+
+Session::put('success', 'Пользователь успешно добавлен.');
+Redirect::to('/public/users');
+exit;
