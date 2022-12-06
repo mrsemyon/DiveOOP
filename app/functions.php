997b339b7db76c4854690eb7fc99ad8bc09edcaa
mrSemyon <syoma4k@gmail.com>
2022-12-06 17:56:57 +0500

    Added function prepareUserPhoto()

---
+++ app/functions.php
@@ -0,0 +1,8 @@
+<?php
+
+function prepareUserPhoto(string $file)
+{
+    $photo = uniqid() . '.' . pathinfo($file['name'], PATHINFO_EXTENSION);
+    move_uploaded_file($file['tmp_name'], $_SERVER['DOCUMENT_ROOT'] . '/upload/' . $photo);
+    return $photo;
+}
--- app/init.php
+++ app/init.php
@@ -4,3 +4,4 @@ session_start();
 
 require $_SERVER['DOCUMENT_ROOT'] . '/app/classes.php';
 require $_SERVER['DOCUMENT_ROOT'] . '/app/config.php';
+require $_SERVER['DOCUMENT_ROOT'] . '/app/functions.php';
