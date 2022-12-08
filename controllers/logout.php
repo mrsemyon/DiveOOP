3f87111056a2c8e4ca3690b7ad93945476900eb5
mrsemyon <syoma4k@gmail.com>
2022-12-09 04:16:31 +0500

    Fixed bug in logout controller

--- controllers/logout.php
+++ controllers/logout.php
@@ -1,14 +1,6 @@
 <?php
 require $_SERVER['DOCUMENT_ROOT'] . '/app/init.php';
 
-$user = QueryBuilder::getInstance()->read('users', ['id' => $_GET['id']]);
-
-if ((Session::get('role') != 'admin') && (Session::get('email') != $user['email'])) {
-    Session::flash('danger', 'У Вас недостаточно прав.');
-    Redirect::to('/public/users');
-    exit;
-}
-
 Session::delete('email');
 Session::delete('role');
 
