14ba38bbbd951b0b344b8552394226dd44537792
mrSemyon <syoma4k@gmail.com>
2022-12-09 23:10:16 +0500

    Fixed bugs in change credentials controller

--- controllers/credentials.php
+++ controllers/credentials.php
@@ -9,39 +9,41 @@ if ((Session::get('role') != 'admin') && (Session::get('email') != $user['email'
     exit;
 }
 
-if (empty($_POST['password']) && ($user['email'] == $_POST['email'])) {
-    Session::flash('danger', 'Информация не была обновлена');
-    Redirect::to('/public/users');
-    exit;
-}
-
 if (empty($_POST['email'])) {
     Session::flash('danger', 'Поле Email не может быть пустым.');
     Redirect::to('/public/credentials', ['id' => $user['id']]);
     exit;
 }
 
-if (empty(QueryBuilder::getInstance()->read('users', ['email' => $_POST['email']]))) {
-    QueryBuilder::getInstance()->update(
-        'users',
-        ['email' => $_POST['email']],
-        ['id' => $_GET['id']]
-    );
-    if (Session::get('role') != 'admin') {
-        Session::put('email', $_POST['email']);
-    }
-    if (!empty($_POST['password'])) {
+if ($user['email'] != $_POST['email']) {
+    if (empty(QueryBuilder::getInstance()->read('users', ['email' => $_POST['email']]))) {
         QueryBuilder::getInstance()->update(
             'users',
-            ['password' => password_hash($_POST['password'], PASSWORD_DEFAULT)],
+            ['email' => $_POST['email']],
             ['id' => $_GET['id']]
         );
+        if (Session::get('role') != 'admin') {
+            Session::put('email', $_POST['email']);
+        }
+        Session::flash('success', 'Регистрационные данные были обновлены.');
+    } else {
+        Session::flash('danger', 'Этот адрес занят.');
+        Redirect::to('/public/credentials', ['id' => $user['id']]);
+        exit;
     }
-    Session::flash('success', 'Регистрационные данные были обновлены.');
-    Redirect::to('/public/users');
-    exit;
 }
 
-Session::flash('danger', 'Этот адрес занят.');
-Redirect::to('/public/credentials', ['id' => $user['id']]);
+if (!empty($_POST['password'])) {
+    QueryBuilder::getInstance()->update(
+        'users',
+        ['password' => password_hash($_POST['password'], PASSWORD_DEFAULT)],
+        ['id' => $_GET['id']]
+    );
+    Session::put('success', 'Регистрационные данные были обновлены.');
+}
+
+if (!Session::exists('success')) {
+    Session::flash('danger', 'Регистрационные данные не были обновлены.');
+}
+Redirect::to('/public/users');
 exit;
