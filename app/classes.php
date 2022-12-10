a83484082bc551b82894974d0cbb89ce39d7465e
mrsemyon <syoma4k@gmail.com>
2022-12-10 16:10:03 +0500

    Added Input class

--- app/classes.php
+++ app/classes.php
@@ -1,6 +1,8 @@
 <?php
+
 require $_SERVER['DOCUMENT_ROOT'] . '/app/classes/Config.php';
 require $_SERVER['DOCUMENT_ROOT'] . '/app/classes/Connection.php';
+require $_SERVER['DOCUMENT_ROOT'] . '/app/classes/Input.php';
 require $_SERVER['DOCUMENT_ROOT'] . '/app/classes/QueryBuilder.php';
 require $_SERVER['DOCUMENT_ROOT'] . '/app/classes/Redirect.php';
 require $_SERVER['DOCUMENT_ROOT'] . '/app/classes/Session.php';
