97b75416cb06b41ac85929a16c04ce861573938d
mrSemyon <syoma4k@gmail.com>
2022-12-13 16:50:03 +0500

    Added validation to registration

--- controllers/registration.php
+++ controllers/registration.php
@@ -1,30 +1,49 @@
 <?php
 require $_SERVER['DOCUMENT_ROOT'] . '/app/init.php';
 
-if (!Token::check(Input::get('token'))) {
-    Session::flash('danger', 'Что-то пошло не так.');
-    Redirect::to('/public/registration');
-    exit;
+if (Input::exists()) {
+    if (Token::check(Input::get('token'))) {
+        $validate = new Validate();
+        $validation = $validate->check($_POST, [
+            'email' => [
+                'required'  => true,
+                'email'     => true,
+                'unique'    => 'users',
+                'min'       => 8,
+            ],
+            'password' => [
+                'required'  => true,
+                'min'       => 3
+            ],
+            'password_again' => [
+                'required'  => true,
+                'matches'   => 'password'
+            ]
+        ]);
+
+        if ($validation->isPassed()) {
+            QueryBuilder::getInstance()->create(
+                'users',
+                [
+                    'email'     => Input::get('email'),
+                    'password'  => password_hash(Input::get('password'), PASSWORD_DEFAULT),
+                    'role'      => 'user',
+                ]
+            );
+
+            Session::put('email', Input::get('email'));
+            Session::put('role', 'user');
+
+            Session::flash('success', 'Вы успешно зарегистрированы.');
+            Redirect::to('/public/users');
+            exit;
+        } else {
+            foreach ($validation->getErrors() as $error) {
+                $errors .= $error . '<br>';
+            }
+            Session::flash('danger', $errors);
+            Redirect::to('/public/registration');
+            exit;
+        }
+    }
 }
-
-if (!empty(QueryBuilder::getInstance()->read('users', ['email' => Input::get('email')]))) {
-    Session::flash('danger', 'Этот эл. адрес уже занят другим пользователем.');
-    Redirect::to('/public/registration');
-    exit;
-}
-
-QueryBuilder::getInstance()->create(
-    'users',
-    [
-        'email'     => Input::get('email'),
-        'password'  => password_hash(Input::get('password'), PASSWORD_DEFAULT),
-        'role'      => 'user',
-    ]
-);
-
-Session::put('email', Input::get('email'));
-Session::put('role', 'user');
-
-Session::flash('success', 'Вы успешно зарегистрированы.');
-Redirect::to('/public/users');
-exit;
