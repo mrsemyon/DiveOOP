676eead3c25fc8eacd745c1656915d80c2843924
mrSemyon <syoma4k@gmail.com>
2022-12-13 16:49:40 +0500

    Added validation to registration

--- controllers/authorization.php
+++ controllers/authorization.php
@@ -1,20 +1,38 @@
 <?php
 require $_SERVER['DOCUMENT_ROOT'] . '/app/init.php';
 
-if (!Token::check(Input::get('token'))) {
-    Session::flash('danger', 'Что-то пошло не так.');
-    Redirect::to('/public/authorization');
-    exit;
-}
+if (Input::exists()) {
+    if (Token::check(Input::get('token'))) {
+        $validate = new Validate();
+        $validation = $validate->check($_POST, [
+            'email' => [
+                'required'  => true,
+                'email'     => true,
+            ],
+            'password' => [
+                'required'  => true,
+            ]
+        ]);
 
-$user = QueryBuilder::getInstance()->read('users', ['email' => Input::get('email')]);
+        if ($validation->isPassed()) {
+            $user = QueryBuilder::getInstance()->read('users', ['email' => Input::get('email')]);
 
-if (!empty($user) && password_verify(Input::get('password'), $user['password'])) {
-    Session::put('email', Input::get('email'));
-    Session::put('role', $user['role']);
-    Session::flash('success', 'Авторизация прошла успешно.');
-    Redirect::to('/public/users');
-    exit;
+            if (!empty($user) && password_verify(Input::get('password'), $user['password'])) {
+                Session::put('email', Input::get('email'));
+                Session::put('role', $user['role']);
+                Session::flash('success', 'Авторизация прошла успешно.');
+                Redirect::to('/public/users');
+                exit;
+            }
+        } else {
+            foreach ($validation->getErrors() as $error) {
+                $errors .= $error . '<br>';
+            }
+            Session::flash('danger', $errors);
+            Redirect::to('/public/authorization');
+            exit;
+        }
+    }
 }
 
 Session::flash('danger', 'Неверное имя пользователя или пароль.');
